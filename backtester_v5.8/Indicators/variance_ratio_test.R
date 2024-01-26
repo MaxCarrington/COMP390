54ba555414c1b7f26603ce68c4aabadfa141445d
MaxCarrington <maxcarrington0002@gmail.com>
2024-01-26 10:21:33 +0000

    Fixed variance ratio test

--- backtester_v5.8/Indicators/variance_ratio_test.R
+++ backtester_v5.8/Indicators/variance_ratio_test.R
@@ -3,13 +3,14 @@
 # Used to test the random walk hypothesis in a time series. The null hypothesis 
 # is that the series is a random walk (in this case, the variance ratio will be 
 # 1). However if there is a notable deviation from 1 the time series may be 
-# mean reverting or trending.
+# mean reverting or trending. If the 
 #-------------------------------------------------------------------------------
 
 # Performs a variance ratio test on a given time series and returns the variance
-# ratio
-performVarianceRatioTest <- function(timeSeries) {
-  testResult <- vrtest::vratio(timeSeries)
-  return(testResult)
+performVarianceRatioTest <- function(series){
+  log_returns <- diff(log(series))
+  log_returns <- na.omit(log_returns)
+  vr_test_result <- Lo.Mac(log_returns, k = 2) # 'k' is the lag parameter
 }
+
 #-------------------------------------------------------------------------------
