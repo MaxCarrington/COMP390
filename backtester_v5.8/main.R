bdaf32cf691c1c628e122b6de2a32b70ef0d6535
MaxCarrington <maxcarrington0002@gmail.com>
2024-04-10 14:21:57 +0100

    fixing jenkins issues

--- backtester_v5.8/main.R
+++ backtester_v5.8/main.R
@@ -13,11 +13,23 @@ source('./set_up_strategies.R')
 dataList <- getData(directory="PART1")
 
 # strategy will be passed in as a command line argument from jenkins
+#mean_reversion = list(stdDev=2, series = c(6,7,8), halfLives = c(48, 42, 50),tradeHistory = list(wins = numeric(), losses = numeric()))
+#market_making=list(series = c(5),lookback = c(20), liquidityThresh = 0.95, windowSize =30, highLiquidityPrdsThresh = 10, volatilityLookback = 10, tradeHistory = list(wins = numeric(), losses = numeric()))
+#momentum=list(series = c(4), lookback = c(12), holdingPeriod = c(12),rsiLookback = c(30), emaLookback = c(30), smaLookback = c(30), wmaLookback = c(30), maThreshold = 0.7, overboughtThresh = 60, oversoldThresh = 40, maType = "SMA",tradeHistory = list(wins = numeric(), losses = numeric()))
 args <- commandArgs(trailingOnly = TRUE)
+params <- list()
 if (length(args) < 1) {
   tradingStrategy <- "mean_reversion"
+  params <- list(stdDev=2, series = c(6,7,8), halfLives = c(48, 42, 50),tradeHistory = list(wins = numeric(), losses = numeric()))
 } else{
  tradingStrategy <- args[1]
+ if(args[1] == "mean_reversion")
+   params <- list(stdDev=2, series = c(6,7,8), halfLives = c(48, 42, 50),tradeHistory = list(wins = numeric(), losses = numeric()))
+ else if(args[1] == "momentum")
+   params <-list(series = c(4), lookback = c(12), holdingPeriod = c(12),rsiLookback = c(30), emaLookback = c(30), smaLookback = c(30), wmaLookback = c(30), maThreshold = 0.7, overboughtThresh = 60, oversoldThresh = 40, maType = "SMA",tradeHistory = list(wins = numeric(), losses = numeric()))
+ else if(args[1] == "market_making")
+   params <- list(series = c(5),lookback = c(20), liquidityThresh = 0.95, windowSize =30, highLiquidityPrdsThresh = 10, volatilityLookback = 10, tradeHistory = list(wins = numeric(), losses = numeric()))
+ 
 }
 
 # check that the choice is valid
@@ -43,10 +55,8 @@ outSampledataList <- lapply(dataList, function(x)
 #strategies <- suitableStratslist(inSampleDataList)
 #params <- setUpTradingParams(tradingStrategy, strategies)
 
-market_making=list(series = c(5),lookback = c(20), liquidityThresh = 0.95, windowSize =30, highLiquidityPrdsThresh = 10, volatilityLookback = 10, tradeHistory = list(wins = numeric(), losses = numeric()))
-momentum=list(series = c(4), lookback = c(12), holdingPeriod = c(12),rsiLookback = c(30), emaLookback = c(30), smaLookback = c(30), wmaLookback = c(30), maThreshold = 0.7, overboughtThresh = 60, oversoldThresh = 40, maType = "SMA",tradeHistory = list(wins = numeric(), losses = numeric()))
-mean_reversion=list(stdDev=2, series = c(6,7,8), halfLives = c(48, 42, 50),tradeHistory = list(wins = numeric(), losses = numeric()))
-load_strategy(tradingStrategy, mean_reversion) # function from example_strategies.R
+
+load_strategy(tradingStrategy, params) # function from example_strategies.R
 
 sMult <- 0.20 # slippage multiplier
 results <- backtest(inSampleDataList,getOrders,params,sMult)
